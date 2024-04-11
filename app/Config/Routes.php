0f4a057b37ebe293b5b708180469c442ac0eeb80
PriyankaMitech <priyanka.mitech@gmail.com>
2024-04-11 17:25:04 +0530

    Vendor and tax master created

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -11,7 +11,27 @@ $routes->post('/', 'Home::index');
 $routes->get('register','Home::register');
 $routes->post('register', 'Home::register');
 
+$routes->get('logout','Home::logout');
+
 $routes->get('admin_dashboard','Home::admin_dashboard');
+
+$routes->get('tax','Home::tax');
+$routes->post('tax','Home::tax');
+$routes->get('edit_tax/(:any)','Home::tax/$1');
+$routes->post('edit_tax/(:any)','Home::tax/$1');
+
+$routes->get('vendor_type','Home::vendor_type');
+$routes->post('vendor_type','Home::vendor_type');
+$routes->get('edit_vendor_type/(:any)','Home::vendor_type/$1');
+$routes->post('edit_vendor_type/(:any)','Home::vendor_type/$1');
+
+$routes->get('delete/(:any)/(:any)','Home::delete/$1');
+// $routes->post('delete','Home::delete');
+
+
+
+
+
 
 
 
